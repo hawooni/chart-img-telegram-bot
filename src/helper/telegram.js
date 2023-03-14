5e6cafbdc2c66fbe2aaa12222cbaaa760c102925
hawooni <hawooni@protonmail.com>
2023-03-14 02:00:40 +0000

    update with logger & setWebhook()

--- src/helper/telegram.js
+++ src/helper/telegram.js
@@ -1,3 +1,20 @@
+import log from './logger'
+
+/**
+ * @param {String} apiToken
+ * @param {String} baseUrl eg. https://example.com
+ * @param {String} secretToken
+ * @param {Integer} maxConn
+ * @returns {Promise}
+ */
+export function setWebhook(apiToken, baseUrl, secretToken, maxConn = 100) {
+  return postPayload(apiToken, 'setWebhook', {
+    url: `${baseUrl}/webhook`,
+    max_connections: maxConn,
+    secret_token: secretToken,
+  })
+}
+
 /**
  * @param {String} apiToken
  * @param {String} method
@@ -5,6 +22,8 @@
  * @returns {Promise}
  */
 export function postPayload(apiToken, method, payload) {
+  log.debug(`telegram.postPayload(apiToken, ${method}, ${JSON.stringify(payload)})`) // prettier-ignore
+
   return fetch(`https://api.telegram.org/bot${apiToken}/${method}`, {
     method: 'POST',
     body: JSON.stringify(payload),
