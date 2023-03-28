7ea22cf43f8a88aae4bf011e429d5baae9f20331
hawooni <hawooni@protonmail.com>
2023-03-28 17:46:56 +0000

    update with message status invalid content-type

--- src/controller/webhook.js
+++ src/controller/webhook.js
@@ -1,9 +1,11 @@
 import procWebhook from '../service/procWebhook'
 import { JsonResponse } from '../helper/response'
 
+import MSG from '../message/status'
+
 export default async (req, env) => {
   if (req.headers.get('Content-Type') !== 'application/json') {
-    return new JsonResponse({ message: 'Invalid Content Type' }, 400)
+    return new JsonResponse({ message: MSG.INVALID_CONTENT_TYPE }, 400)
   } else {
     await procWebhook(await req.json(), env)
     return new JsonResponse()
--- src/message/status.js
+++ src/message/status.js
@@ -1,5 +1,6 @@
 export default {
   INVALID_REQUEST: 'Invalid Request', // 400
+  INVALID_CONTENT_TYPE: 'Invalid Content Type', // 400
   ROUTE_NOT_FOUND: 'Route Not Found', // 404
   UNKNOWN_ERROR: 'Something Went Wrong', // 500
 }
